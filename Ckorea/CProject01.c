a20379ac117a22be9b1d91a483f6977154ceff9d
pray asdasd <ehangud123@naver.com>
2022-04-23 10:49:39 +0900

    작성자 날짜

--- Ckorea/CProject01.c
+++ Ckorea/CProject01.c
@@ -1,7 +1,7 @@
 //프로그래밍 언어 : 정해진 규칙으로 작성하면 기계에게 일을 시킬 수 있는것
 #include <stdio.h>												// printf 화면에 보여준느 기능을 사용하기 위해서 추가해야하는 파일
 
-void main(){															// main : 시작위치  
+void main1(){															// main : 시작위치  
 	printf("안녕하세요~C언어");
 	printf("Hello CWorld");
 }																			// 중괄호 : 범위
---
+++ Ckorea/CProject02.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void main() {
+	printf("작성자 : 이재현");
+	printf("\n날짜 : 2022-04-23");
+	printf("\nprintf 사용하기");
+}
